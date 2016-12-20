087cc8ad28bacaa3da2a0a435bb7591c30d3f642
Chris McGehee <chris.a.mcgehee@gmail.com>
2016-12-19 22:19:37 -0800

    Automating the release process a little bit more

---
+++ VoiceCommandInstaller/CopyReleaseFiles.bat
@@ -0,0 +1,7 @@
+xcopy "VoiceCommand.dll" "../../Release Files\" /Y
+xcopy "OpenDentBusiness.dll" "../../Release Files\" /Y 
+xcopy "CDT.dll" "../../Release Files\" /Y 
+xcopy "MySql.Data.dll" "../../Release Files\" /Y 
+xcopy "Oracle.DataAccess.dll" "../../Release Files\" /Y 
+xcopy "CodeBase.dll" "../../Release Files\" /Y 
+xcopy "VoiceCommandInstaller.exe" "../../Release Files\" /Y 
