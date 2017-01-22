6e1a8a02368e2773cc4c35202c88a1571fc13202
Chris McGehee <chris.a.mcgehee@gmail.com>
2017-01-21 17:02:24 -0800

    Automatically copy VoiceCommand.dll to beta and stable bin folders

--- VoiceCommand/CopyDllToOD.bat
+++ VoiceCommand/CopyDllToOD.bat
@@ -1,2 +1,6 @@
 xcopy "C:\development\OpenDentalVoice\VoiceCommand\bin\Debug\VoiceCommand.dll" "C:\development\OPEN DENTAL SUBVERSION\head\OpenDental\bin\Debug" /Y
 xcopy "C:\development\OpenDentalVoice\VoiceCommand\bin\Release\VoiceCommand.dll" "C:\development\OPEN DENTAL SUBVERSION\head\OpenDental\bin\Release" /Y
+xcopy "C:\development\OpenDentalVoice\VoiceCommand\bin\Debug\VoiceCommand.dll" "C:\development\OPEN DENTAL SUBVERSION\openDental16.4\OpenDental\bin\Debug" /Y
+xcopy "C:\development\OpenDentalVoice\VoiceCommand\bin\Release\VoiceCommand.dll" "C:\development\OPEN DENTAL SUBVERSION\opendental16.4\OpenDental\bin\Release" /Y
+xcopy "C:\development\OpenDentalVoice\VoiceCommand\bin\Debug\VoiceCommand.dll" "C:\development\OPEN DENTAL SUBVERSION\opendental16.3\OpenDental\bin\Debug" /Y
+xcopy "C:\development\OpenDentalVoice\VoiceCommand\bin\Release\VoiceCommand.dll" "C:\development\OPEN DENTAL SUBVERSION\opendental16.3\OpenDental\bin\Release" /Y
