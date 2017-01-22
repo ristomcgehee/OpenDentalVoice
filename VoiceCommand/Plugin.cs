067c88dad13e7b56cd25b809cc392c46914f8bac
Chris McGehee <chris.a.mcgehee@gmail.com>
2017-01-21 18:35:32 -0800

    Voice was still responding after the Perio Chart had been closed.

--- VoiceCommand/Plugin.cs
+++ VoiceCommand/Plugin.cs
@@ -5,11 +5,16 @@ using OpenDental;
 namespace VoiceCommand {//The namespace for this class must match the dll filename, including capitalization.  All other classes will typically belong to the same namespace too, but that's not a requirement.
 	/// <summary>Required class.  Don't change the name.</summary>
 	public class Plugin : PluginBase {
+		private static FormPerioV _formPerioV;
 		
 		public override bool HookAddCode(object sender,string hookName,params object[] parameters) {//required method
 			switch(hookName) {
 				case "FormPerio.Load_end":
-					new FormPerioV((FormPerio)sender).InitializeListening();
+					_formPerioV=new FormPerioV((FormPerio)sender);
+					_formPerioV.InitializeListening();
+					return true;
+				case "FormPerio.Closing_end":
+					_formPerioV?.Dispose();
 					return true;
 				default:
 					return false;
