640d75ca0720a763bd097d82bd0504a655d06565
Chris McGehee <chris.a.mcgehee@gmail.com>
2017-02-04 12:51:08 -0800

    Adding GPL license to the project

--- VoiceCommand/VoiceMsgBox.cs
+++ VoiceCommand/VoiceMsgBox.cs
@@ -1,4 +1,21 @@
-﻿using OpenDental;
+﻿/*==================================================================================
+This file is a part of Voice Command.
+Copyright 2017  Christopher A. McGehee
+
+Voice Command is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Voice Command is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+===================================================================================*/
+using OpenDental;
 using System.Windows.Forms;
 
 namespace VoiceCommand {
